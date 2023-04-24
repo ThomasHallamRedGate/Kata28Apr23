c2038db40f17d06a32026b5cb070ec9fd31b9ae7
Thomas Hallam <thomas.hallam@red-gate.com>
2023-04-24 15:17:15 +0100

    Implement single forward command

--- Kata28Apr23/Rover.cs
+++ Kata28Apr23/Rover.cs
@@ -1,14 +1,29 @@
+using System.Diagnostics;
+
 namespace Kata28Apr23;
 
 public class Rover
 {
+    private readonly Direction _direction;
+
     public Rover(Position position, Direction direction)
     {
-        
+        _direction = direction;
+        Position = position;
     }
+
+    public Position Position { get; private set; }
 
     public void ProcessCommands(char[] commands)
     {
         
+        Position = _direction switch
+        {
+            Direction.North => Position with { Y = Position.Y + 1 },
+            Direction.East => Position with { X = Position.X + 1 },
+            Direction.South => Position with { Y = Position.Y - 1 },
+            Direction.West => Position with { X = Position.X - 1 },
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 }
--- Kata28Apr23/UnitTest1.cs
+++ Kata28Apr23/UnitTest1.cs
@@ -7,9 +7,22 @@ public class Tests
     {
     }
 
-    [Test]
-    public void Test1()
+    [TestCase(0, 0, Direction.North, new [] {'f'}, 0, 1)]
+    [TestCase(0, 0, Direction.East, new [] {'f'}, 1, 0)]
+    [TestCase(0, 0, Direction.South, new [] {'f'}, 0, -1)]
+    [TestCase(0, 0, Direction.West, new [] {'f'}, -1, 0)]
+    public void ProcessCommands_UpdatesPositionCorrectly(
+        double initialX, 
+        double initialY,
+        Direction direction,
+        char[] commands,
+        double expectedX,
+        double expectedY)
     {
-        Assert.Pass();
+        var rover = new Rover(new Position(initialX, initialY), direction);
+        
+        rover.ProcessCommands(commands);
+        
+        Assert.That(rover.Position, Is.EqualTo(new Position(expectedX, expectedY)));
     }
 }
